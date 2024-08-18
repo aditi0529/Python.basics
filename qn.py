e2bd682b1233dada80c6683f75658aff82f88fa5
aditi0529 <165752635+aditi0529@users.noreply.github.com>
2024-08-18 20:29:43 +0530

    Add files via upload
    #this are basic practice questions of python.

---
+++ qn.py
@@ -0,0 +1,14 @@
+#write a program to print twinkle twinkle little star poem in python
+def print_poem():
+    poem = """Twinkle, twinkle, little star,
+    How I wonder what you are!
+        Up above the world so high,
+        Like a diamond in the sky.
+
+Twinkle, twinkle, little star,
+    How I wonder what you are!"""
+    
+    print(poem)
+
+# Call the function to print the poem
+print_poem()
